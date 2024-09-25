4ea8153d6911ba09ccbd6c22d7609d0075a0745d
Santhosh13072006 <santhoshreddy1307@gmail.com>
2024-09-25 16:36:23 +0530

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,a,h;
+    char A,B,C,D,E;
+    
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            switch(j)
+            {
+                case 1:{
+                       h=65;
+                       break;
+                       }
+                case 2:{
+                       h=66;
+                       break;
+                       }
+                case 3:{
+                       h=67;
+                       break;
+                       }
+                case 4:{
+                       h=68;
+                       break;
+                       }
+                default:{
+                        h=69;
+                       }
+            }
+            if(i>=j)
+             {
+                     
+                 printf("%c",h);
+                          
+                     
+             }
+        }
+        printf("\n");
+        
+        
+    }
+     return 0;
+}
